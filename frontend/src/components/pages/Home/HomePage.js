20545c3d625b27678c74080a1f029772af51d7b3
bawumbila <74751215+bawumbila@users.noreply.github.com>
2024-01-06 03:17:08 -0500

    added css function for displaying properties

--- frontend/src/components/pages/Home/HomePage.js
+++ frontend/src/components/pages/Home/HomePage.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useEffect, useReducer } from "react";
+import Thumbnails from "../../Thumbnails/Thumbnails";
+import { getAll } from "../../../services/foodService";
+
+const initialState = { foods: [] };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FOODS_LOADED":
+      return { ...state, foods: action.payload };
+    default:
+      return state;
+  }
+};
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { foods } = state;
+
+  useEffect(() => {
+    getAll().then((foods) =>
+      dispatch({ type: "FOODS_LOADED", payload: foods })
+    );
+  }, []);
+
+  return (
+    <>
+      <Thumbnails foods={foods} />
+    </>
+  );
 }
