2a166be154ac54d15c4b4ad1eed6dd6d521df574
bawumbila <74751215+bawumbila@users.noreply.github.com>
2024-01-08 22:23:52 -0500

    Added cart page

--- frontend/src/components/AppRoutes.js
+++ frontend/src/components/AppRoutes.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/Home/HomePage";
+import CartPage from "../pages/Cart/CartPage";
 import FoodPage from "../pages/Food/FoodPage";
+import HomePage from "./pages/Home/HomePage";
 
 export default function AppRoutes() {
   return (
@@ -10,6 +11,8 @@ export default function AppRoutes() {
       <Route path="/search/:searchTerm" element={<HomePage />} />
       <Route path="/tag/:tag" element={<HomePage />} />
       <Route path="/food/:id" element={<FoodPage />} />
+      <Route path="/food/:id" element={<FoodPage />} />
+      <Route path="/cart" element={<CartPage />} />
     </Routes>
   );
 }
